ae824f549ae5b3c9726b64f2699548a93d186e2b
isus203 <128573607+isus203@users.noreply.github.com>
2025-12-14 03:08:21 +0300

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -267,22 +267,22 @@ SHRP_DEV_USE_HEX := true
 SHRP_DARK := true
 
 # Setting up custom addons folder
-SHRP_EXTERNAL_ADDON_PATH := "device/tecno/KH7n/addon"
+SHRP_EXTERNAL_ADDON_PATH := device/tecno/KH7n/addon
 
 # Added treblize addon
-SHRP_EXTERNAL_ADDON_1_NAME := "Imei Backup"
-SHRP_EXTERNAL_ADDON_1_INFO := "Backup Imei to main memory"
-SHRP_EXTERNAL_ADDON_1_FILENAME := "Imei-Backup.zip"
-SHRP_EXTERNAL_ADDON_1_BTN_TEXT := "Backup"
-SHRP_EXTERNAL_ADDON_1_SUCCESSFUL_TEXT := "For backup"
+SHRP_EXTERNAL_ADDON_1_NAME := Imei Backup
+SHRP_EXTERNAL_ADDON_1_INFO := Backup Imei to main memory
+SHRP_EXTERNAL_ADDON_1_FILENAME := Imei-Backup.zip
+SHRP_EXTERNAL_ADDON_1_BTN_TEXT := Backup
+SHRP_EXTERNAL_ADDON_1_SUCCESSFUL_TEXT := For backup
 SHRP_INC_IN_REC_EXTERNAL_ADDON_1 := true
 
 # Added detreblize addon
-SHRP_EXTERNAL_ADDON_2_NAME := "Imei Backup sd"
-SHRP_EXTERNAL_ADDON_2_INFO := "Backup Imei to SD card"
-SHRP_EXTERNAL_ADDON_2_FILENAME := "Imei-Backup_sd.zip"
-SHRP_EXTERNAL_ADDON_2_BTN_TEXT := "Backup"
-SHRP_EXTERNAL_ADDON_2_SUCCESSFUL_TEXT := "For backup"
+SHRP_EXTERNAL_ADDON_2_NAME := Imei Backup sd
+SHRP_EXTERNAL_ADDON_2_INFO := Backup Imei to SD card
+SHRP_EXTERNAL_ADDON_2_FILENAME := Imei-Backup_sd.zip
+SHRP_EXTERNAL_ADDON_2_BTN_TEXT := Backup
+SHRP_EXTERNAL_ADDON_2_SUCCESSFUL_TEXT := For backup
 SHRP_INC_IN_REC_EXTERNAL_ADDON_2 := true
 
 # Including Magisk into recovery ramdisk
