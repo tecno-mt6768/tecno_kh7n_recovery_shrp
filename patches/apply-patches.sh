8ad85497d2dd1652321a13fb9e7a873413dcac25
isus203 <128573607+isus203@users.noreply.github.com>
2025-12-14 03:33:16 +0300

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 cd ./system/core/fs_mgr/
-for f in ../../device/tecno/KH7n/patches/0001-revert-to-fscrypt.patch
-do
-  patch -p1 < $f
-done
+patch -p1 < ../../device/tecno/KH7n/patches/0001-revert-to-fscrypt.patch
 cd ../..
